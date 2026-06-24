386be30f360b979af288079a642d041200616cbe
Your Name <you@example.com>
2026-06-25 07:04:32 +0800

    chore: rename demo app display name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'pl.pwmarcz.autotable.android',
-  appName: 'Autotable',
+  appName: '江南麻将3D',
   webDir: 'build',
   android: {
     allowMixedContent: true,
