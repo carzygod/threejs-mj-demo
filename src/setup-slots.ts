8a6fef3a78b36cd22c7dd83ff2dd5692fdb4295f
Your Name <you@example.com>
2026-06-26 06:29:12 +0800

    Revert "fix: raise right player meld area"
    This reverts commit 01eec0a0b93c20fdb5b1106a5491539108eede54.

--- src/setup-slots.ts
+++ src/setup-slots.ts
@@ -98,11 +98,6 @@ function seats(which?: Array<number>): SlotOp {
   };
 }
 
-function translateSlot(slot: Slot, offset: Vector3): void {
-  slot.origin.add(offset);
-  slot.places = slot.rotations.map(slot.makePlace.bind(slot));
-}
-
 const START: Record<string, Slot> = {
   'hand': new Slot({
     name: 'hand',
@@ -305,13 +300,6 @@ export const SLOT_GROUPS: Record<GameType, Array<SlotGroup>> = {
 
 function fixupSlots(slots: Array<Slot>, gameType: GameType): void {
   for (const slot of slots) {
-    if (
-      (gameType === GameType.FOUR_PLAYER || gameType === GameType.FOUR_PLAYER_DEMO) &&
-      slot.group === 'meld' &&
-      slot.seat === 1
-    ) {
-      translateSlot(slot, new Vector3(0, 14, 0));
-    }
     if (slot.name.startsWith('discard.extra')) {
       slot.linkDesc.requires = `discard.2.5@${slot.seat}`;
     }
